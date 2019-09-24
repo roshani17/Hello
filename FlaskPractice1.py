1a6856da65a2eb35556c4a5b031303d6d50eb722
roshani17 <55732397+roshani17@users.noreply.github.com>
2019-09-24 12:58:23 +0530

    FlaskPractice1 updated

--- FlaskPractice1.py
+++ FlaskPractice1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def Say_Hello():
-    return render_template('SayHello.html')
+    return 'Hello World !!'
 
 if __name__ == '__main__':
     app.run(debug=True)
